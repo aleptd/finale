69e8db77421dbd4c879f989396ceb34ac3ca0665
Nadia1996 <nady-mitika96@hotmail.it>
2020-07-02 10:28:07 +0200

    aggiunto parte geocoding e reverse geocoding

--- app/src/main/java/com/example/autenticazione/MapActivity.java
+++ app/src/main/java/com/example/autenticazione/MapActivity.java
@@ -9,14 +9,23 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,13 +35,28 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -44,8 +68,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     LocationManager locationManager;
     LocationListener locationListener;
 
+    EditText etIndirizzo;
+
+
 
     LatLng posizioneUtente;
+    DownloadTask task;
+
+    PolylineOptions plo;
+
+    List<LatLng> polylinePoints = new LinkedList<LatLng>();
+
+    Geocoder geocoder;
+
 
 
 
@@ -55,6 +90,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        geocoder = new Geocoder(MapActivity.this, Locale.getDefault()); //per inizializzare geocoder
 
         initUI();
 
@@ -64,20 +100,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         verifyPermissions();
 
+        viewLocationFromAddressInEditText();
+
 
     }
 
-    public void initUI(){
-        ibBack=findViewById(R.id.ibBack);
+    public void initUI() {
+        ibBack = findViewById(R.id.ibBack);
     }
 
-    public void goBackToMainActivity (View v) {
+    public void goBackToMainActivity(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
 
-    private void createMapView(Bundle savedInstanceState){
+    private void createMapView(Bundle savedInstanceState) {
         Bundle mapViewBundle = null;
         if (savedInstanceState != null)
             //se savedInstanceState è diverso da null si valorizza mapViewBundle
@@ -121,10 +159,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onLowMemory();
         mapView.onLowMemory();
     }
-    Marker[] markers= new Marker[1000];
+
+    Marker[] markers = new Marker[1000];
+
     @Override
     //la nostra applicazione resta in attesa finchè non arriva una mappa
     public void onMapReady(GoogleMap googleMap) {
+
+        /*
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
 
         DatabaseReference myRef = database.getReference().child("markerList");
@@ -155,10 +198,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                 String name = (String)keyValueFinalObj.get("name");
 
                                 markers[i-1]= new Marker(id_marker,lat,longi,true,description,name);
-          /* }else {
-                Map keyValueFinalObj = (Map) entry.getValue();
-                type = (String) entry.getKey();
-            }*/
+
 
                             }
                             i++;
@@ -172,6 +212,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         //handle databaseError
                     }
                 });
+
+        */
+
         gmap = googleMap;
 
         //per visualizzare il traffico nella mappa
@@ -208,9 +251,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         //si aggiunge un marker in versione semplificata
         gmap.addMarker(new MarkerOptions().position(city)
-        .title("sei qui")
-        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-
+                .title("sei qui")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+/*
         for(int k= 0; k< markers.length; k++){
             //si aggiunge un marker in versione semplificata
             gmap.addMarker(new MarkerOptions().position(new LatLng(markers[k].getLongi(), markers[k].getLat()))
@@ -220,11 +263,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
 
         }
-
+*/
 
         // si muove la camera su il punto LatLng definito
         //senza zoom
-     //   gmap.moveCamera(CameraUpdateFactory.newLatLng(city));
+        //   gmap.moveCamera(CameraUpdateFactory.newLatLng(city));
 
         //con zoom
         //si imposta il livello di zoom manualmente
@@ -236,7 +279,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     LOCATION
      */
 
-    public void getLocation(){
+    public void getLocation() {
         // creo un nuovo Location Manager
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -251,23 +294,22 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Log.i("mylog", location.getLatitude() + ", " + location.getLongitude());
 
 
-
                 posizioneUtente = new LatLng(location.getLatitude(), location.getLongitude());
                 displayUserPosition(posizioneUtente);
 
-                // vecchio, utilizzato solo per costruire la soluzione
 
-                //LatLng destinazione = new LatLng(45.061892, 7.661223);
-                //calcolaPercorso(destinazione);
-/*
-                if (polylinePoints != null && polylinePoints.size() >0){
+
+                // vecchio, utilizzato solo per costruire la soluzione
+                /*LatLng destinazione = new LatLng(45.061892, 7.661223);
+                calcolaPercorso(destinazione);*/
+
+                if (polylinePoints != null && polylinePoints.size() >0){ //quindi abbiamo almeno un punto
                     polylinePoints.remove(0);
                     polylinePoints.add(0, posizioneUtente);
                     drawPolylines();
                 }
 
                 reverseGeocoding(posizioneUtente);
-                */
 
 
             }
@@ -296,14 +338,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // controllo se i permessi del Manifest sono di tipo ACCESS_FINE_LOCATION
         // e se non ce li fornisce
 
-        if (Build.VERSION.SDK_INT >= 23){
+        if (Build.VERSION.SDK_INT >= 23) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // se l'utente non ha fornito i permessi, glieli chiedo
                 //il requestcode comunica quale richiesta è stata effettuata
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }else
+            } else
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, locationListener);
-        }else
+        } else
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, locationListener);
 
     }
@@ -327,12 +369,215 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     }
 
-    public void displayUserPosition(LatLng posizioneUtente){
+    public void displayUserPosition(LatLng posizioneUtente) {
         //gmap.clear();
-        gmap.addMarker(new MarkerOptions().position(posizioneUtente));
+        //da qui si possono specificare tutta una serie di info sul marker dell'utente aggiungendo un punto dopo .position
+        gmap.addMarker(new MarkerOptions().position(posizioneUtente)
+                .title("posto"));
         gmap.moveCamera(CameraUpdateFactory.newLatLng(posizioneUtente));
+
+    }
+
+    /*
+    Calcolo percorso
+     */
+
+    public void calcolaPercorso(LatLng destinazione) {  //calcola il percorso dalla posizione dell'utente dove si trova in questo istante alla destinazione
+        //per ripulire la lista di polylinePoints
+        polylinePoints.clear();
+        //stiamo aggiungendo la posizione dell'utente attuale--> il punto di partenza di tutto il percorso
+         polylinePoints.add(posizioneUtente);  //è una lista
+
+        task = new DownloadTask();
+        //per eseguire il task
+        //si passa l'url per il calcolo del percorso
+        task.execute("https://maps.googleapis.com/maps/api/directions/json?origin=" +
+                posizioneUtente.latitude + "," + posizioneUtente.longitude +
+                "&destination=" +
+                destinazione.latitude + "," + destinazione.longitude +
+                "&key=AIzaSyD9xPAfQ78vrBHzNgBby4slgrdLTjh1k4M");
+
+    }
+
+    public void drawPolylines(){
+        plo = new PolylineOptions();
+
+        for(LatLng latLng : polylinePoints){
+            plo.add(latLng);
+            plo.color(Color.RED);
+            plo.width(10); //per ingrandire lo spessore della riga
+        }
+
+        gmap.addPolyline(plo); // si aggiunge la polyline alla mappa
+    }
+
+  /*
+    Reverse Geocoding
+     */
+
+    private void reverseGeocoding(LatLng coordinate){
+
+        try{
+
+            List<Address> addresses = geocoder.getFromLocation(coordinate.latitude, coordinate.longitude, 1);
+            Log.i("indirizzo", addresses.toString());
+
+            //bisogna estrarre un indirizzo specifico in formato stringa da questa lista di indirizzi
+
+            String indirizzo = addresses.get(0).getAddressLine(0);
+            Toast.makeText(this, "Ecco l'indirizzo in cui ti trovi "+indirizzo, Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //metodo che ci permette di leggere l'etichetta modificata
+
+    private void viewLocationFromAddressInEditText(){
+        etIndirizzo = findViewById(R.id.indirizzo);
+
+
+        //stiamo aggiungendo un listener che verifica quando è modificata l'editText
+        etIndirizzo.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) || actionId == EditorInfo.IME_ACTION_DONE){
+                    Log.i("mylog", "Enter premuto");
+
+                    LatLng destinazione = geocoding(etIndirizzo.getText().toString());
+                    Log.i("mylog", destinazione.toString());
+
+                    gmap.clear();
+                    gmap.addMarker(new MarkerOptions().position(destinazione).title("destinazione"));
+                    gmap.moveCamera(CameraUpdateFactory.newLatLng(destinazione)); //per muovere la camera
+
+                    calcolaPercorso(destinazione); //prende la posizione dell'utente e calcola il percorso fino a destinazione
+                }
+
+                return false;
+            }
+        });
+    }
+// Geocoding
+
+    private LatLng geocoding(String indirizzo){
+
+        List<Address> addresses ;
+        LatLng destinazione; //ossia il nostro indirizzo
+
+        try{
+            addresses = geocoder.getFromLocationName(indirizzo, 5); //stiamo speicificando 5 risultati possibili
+
+            if (addresses == null)  //se è null usciamo dal metodo --> non vi è nessuna coppia di LatLng
+                return null;
+
+            //se non è null ci facciamo restituire la prima posizione della prima lista di indirizzi (location)
+
+            Address location = addresses.get(0);
+
+            destinazione = new LatLng(location.getLatitude(), location.getLongitude());
+            return destinazione;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+ /*
+    DownloadTask
+     */
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpsURLConnection urlConnection;
+
+            //quando creo il task gli passo un URL da contattare, al quale si vuole inviare la richiesta
+
+            try {
+                url = new URL(strings[0]); //questo metodo può generare una ricezione
+                urlConnection = (HttpsURLConnection) url.openConnection(); //da qui si apre la connessione verso l'URL
+
+                //per ricevere i dati dall'urlConnection
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) { //ossia affinchè si hanno dei dati da leggere
+                    char cur = (char) data; //si possono leggere i dati carattere per carattere
+                    result += cur;
+                    data = reader.read();
+                }
+
+                Log.i("mylog", result);
+                return result; //se si è riusciti a leggere tutti i dati si ritorna result
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String stringa) {
+            super.onPostExecute(stringa);
+
+            try {
+                //si trasforma l'oggetto in stringa
+                JSONObject jsonObject = new JSONObject(stringa);
+
+                String routes = jsonObject.getString("routes");
+
+                JSONArray arrayRoutes = new JSONArray(routes);
+                //prendiamo la prima routes, immaginando sia proprio quella che ci interessi
+                JSONObject primaRoute = arrayRoutes.getJSONObject(0);
+
+                String legs = primaRoute.getString("legs");
+                JSONArray arrayLegs = new JSONArray(legs);
+                JSONObject primaLeg = arrayLegs.getJSONObject(0);
+
+                String steps = primaLeg.getString("steps");
+                JSONArray arraySteps = new JSONArray(steps);
+
+                //all'interno del ciclo for ci prendiamo i jsonobject relativi alla posizione i-esima dell'arraysteps
+
+                for (int i = 0; i<arraySteps.length(); i++){
+                    JSONObject step = arraySteps.getJSONObject(i); //stiamo leggendo uno per uno i singoli elementi
+                    String lat = step.getJSONObject("end_location").getString("lat");
+                    String lon = step.getJSONObject("end_location").getString("lng");
+
+                    Log.i("mylog", lat+" "+lon);
+                   polylinePoints.add(new LatLng(Double.parseDouble(lat), Double.parseDouble(lon)));
+
+                }
+
+
+                drawPolylines();
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
 
 }
+
