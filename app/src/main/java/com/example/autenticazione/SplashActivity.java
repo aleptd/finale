ae0df204723d78254d4c6c9df42ef95ff577db4d
Nadia1996 <nady-mitika96@hotmail.it>
2020-07-04 23:14:46 +0200

    inserimento splash screen

---
+++ app/src/main/java/com/example/autenticazione/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.example.autenticazione;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+   // private Handler mHandler;
+   // private  Runnable mRunnable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+/*
+        mRunnable= new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        };
+        mHandler= new Handler();
+        mHandler.postDelayed(mRunnable,2000);
+
+
+
+        }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHandler != null && mRunnable !=null)
+            mHandler.removeCallbacks(mRunnable);
+    */
+    }
+}
+
